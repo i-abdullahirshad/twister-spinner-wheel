de2b9e66ae2ccc68a1b639735e1c2d0d232610e7
Abdullah Irshad <139970099+i-abdullahirshad@users.noreply.github.com>
2026-04-22 05:19:38 +0500

    Update PrivacyPolicy.tsx

--- src/pages/PrivacyPolicy.tsx
+++ src/pages/PrivacyPolicy.tsx
@@ -169,7 +169,7 @@ const PrivacyPolicy = () => {
 
           <h3 className="text-xl font-semibold text-foreground mt-8 mb-3">Transfer of Your Personal Data</h3>
           <p>
-            Your information, including Personal Data, is processed at the Company's operating offices and in any other places where the parties involved in the processing are located. It means that this information may be transferred to — and maintained on — computers located outside of Your state, province, country or other governmental jurisdiction where the data protection laws may differ from those from Your jurisdiction.
+            Your information, including Personal Data, is processed at the Company's operating offices and in any other places where the parties involved in the processing are located. It means that this information may be transferred to - and maintained on - computers located outside of Your state, province, country or other governmental jurisdiction where the data protection laws may differ from those from Your jurisdiction.
           </p>
           <p>
             Where required by applicable law, We will ensure that international transfers of Your Personal Data are subject to appropriate safeguards and supplementary measures where appropriate. The Company will take all steps reasonably necessary to ensure that Your data is treated securely and in accordance with this Privacy Policy and no transfer of Your Personal Data will take place to an organization or a country unless there are adequate controls in place including the security of Your data and other personal information.
